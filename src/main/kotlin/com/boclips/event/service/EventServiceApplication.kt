a536070936e730ef36166635b75252f570f9dab1
Jacek Rzeniewicz <jacek@boclips.com>
2019-05-10 11:43:52 +0200

    Enable pubsub

--- src/main/kotlin/com/boclips/event/service/EventServiceApplication.kt
+++ src/main/kotlin/com/boclips/event/service/EventServiceApplication.kt
@@ -1,9 +1,11 @@
 package com.boclips.event.service
 
+import com.boclips.events.spring.EnableBoclipsEvents
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBoclipsEvents(appName = "event-service")
 class EventServiceApplication
 
 fun main(args: Array<String>) {
