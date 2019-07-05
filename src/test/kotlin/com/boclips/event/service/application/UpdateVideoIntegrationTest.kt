5d7f389adcc2814f004da9bca11cfe7a27c205b8
Jacek Rzeniewicz <jacek@boclips.com>
2019-07-05 17:03:31 +0100

    Clean up imports

--- src/test/kotlin/com/boclips/event/service/application/UpdateVideoIntegrationTest.kt
+++ src/test/kotlin/com/boclips/event/service/application/UpdateVideoIntegrationTest.kt
@@ -2,13 +2,9 @@ package com.boclips.event.service.application
 
 import com.boclips.event.service.infrastructure.DatabaseConstants
 import com.boclips.event.service.testsupport.AbstractSpringIntegrationTest
-import com.boclips.event.service.testsupport.TestFactories
 import com.boclips.event.service.testsupport.TestFactories.createVideoUpdates
-import com.sun.tools.corba.se.idl.som.cff.Messages.msg
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.bson.Document
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class UpdateVideoIntegrationTest : AbstractSpringIntegrationTest() {
