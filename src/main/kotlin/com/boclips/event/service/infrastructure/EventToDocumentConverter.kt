4fccddf3bd1e3fe867b6bbb71b362961af712d74
Jacek Rzeniewicz <jacek@boclips.com>
2019-08-07 18:16:14 +0100

    Persist playback device id when present

--- src/main/kotlin/com/boclips/event/service/infrastructure/EventToDocumentConverter.kt
+++ src/main/kotlin/com/boclips/event/service/infrastructure/EventToDocumentConverter.kt
@@ -37,6 +37,7 @@ object EventToDocumentConverter {
                 + ("videoDurationSeconds" to videoSegmentPlayed.videoDurationSeconds)
                 + ("videoIndex" to videoSegmentPlayed.videoIndex)
                 + ("videoId" to videoSegmentPlayed.videoId)
+                + ("consumerDevice" to videoSegmentPlayed.consumerDevice)
         )
     }
 
