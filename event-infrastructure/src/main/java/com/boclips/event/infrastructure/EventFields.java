f3bf2718fda81bf6f13dcf478a764361e56965ab
Jacek Rzeniewicz <jacek@rzrz.pl>
2020-06-11 10:11:00 +0100

    Extract event fields to constants

--- event-infrastructure/src/main/java/com/boclips/event/infrastructure/EventFields.java
+++ event-infrastructure/src/main/java/com/boclips/event/infrastructure/EventFields.java
@@ -1,6 +1,65 @@
 package com.boclips.event.infrastructure;
 
 public class EventFields {
-    public final static String DEVICE_ID = "deviceId";
+
+    public static class Type {
+        public final static String RESOURCES_SEARCHED = "RESOURCES_SEARCHED";
+        public final static String VIDEOS_SEARCHED = "VIDEOS_SEARCHED";
+        public final static String VIDEO_SEGMENT_PLAYED = "VIDEO_SEGMENT_PLAYED";
+        public final static String VIDEO_PLAYER_INTERACTED_WITH = "VIDEO_PLAYER_INTERACTED_WITH";
+        public final static String VIDEO_ADDED_TO_COLLECTION = "VIDEO_ADDED_TO_COLLECTION";
+        public final static String VIDEO_REMOVED_FROM_COLLECTION = "VIDEO_REMOVED_FROM_COLLECTION";
+        public final static String VIDEO_INTERACTED_WITH = "VIDEO_INTERACTED_WITH";
+        public final static String COLLECTION_BOOKMARK_CHANGED = "COLLECTION_BOOKMARK_CHANGED";
+        public final static String COLLECTION_VISIBILITY_CHANGED = "COLLECTION_VISIBILITY_CHANGED";
+        public final static String COLLECTION_SUBJECTS_CHANGED = "COLLECTION_SUBJECTS_CHANGED";
+        public final static String COLLECTION_AGE_RANGE_CHANGED = "COLLECTION_AGE_RANGE_CHANGED";
+        public final static String COLLECTION_INTERACTED_WITH = "COLLECTION_INTERACTED_WITH";
+        public final static String USER_EXPIRED = "USER_EXPIRED";
+        public final static String PLATFORM_INTERACTED_WITH = "PLATFORM_INTERACTED_WITH";
+        public final static String PAGE_RENDERED = "PAGE_RENDERED";
+    }
+
+    public final static String TYPE = "type";
+    public final static String SUBTYPE = "subtype";
+    public final static String PAYLOAD = "payload";
+
+    public final static String TIMESTAMP = "timestamp";
+
+    public final static String VIDEO_ID = "videoId";
+    public final static String COLLECTION_ID = "collectionId";
+
+    public final static String USER_ID = "userId";
     public final static String EXTERNAL_USER_ID = "externalUserId";
+    public final static String DEVICE_ID = "deviceId";
+
+    public final static String ORGANISATION_ID = "organisationId";
+    public final static String ORGANISATION_TYPE = "organisationType";
+    public final static String ORGANISATION_PARENT_ID = "organisationParentId";
+    public final static String ORGANISATION_PARENT_TYPE = "organisationParentType";
+
+    public final static String URL = "url";
+
+    public final static String PLAYBACK_VIDEO_INDEX = "videoIndex";
+    public final static String PLAYBACK_SEGMENT_START_SECONDS = "segmentStartSeconds";
+    public final static String PLAYBACK_SEGMENT_END_SECONDS = "segmentEndSeconds";
+
+    public final static String PLAYER_INTERACTED_WITH_CURRENT_TIME = "currentTime";
+
+    public final static String SEARCH_QUERY = "query";
+    public final static String SEARCH_RESOURCE_TYPE = "resourceType";
+    public final static String SEARCH_RESULTS_PAGE_INDEX = "pageIndex";
+    public final static String SEARCH_RESULTS_PAGE_SIZE = "pageSize";
+    public final static String SEARCH_RESULTS_PAGE_RESOURCE_IDS = "pageResourceIds";
+    public final static String SEARCH_RESULTS_PAGE_VIDEO_IDS = "pageVideoIds";
+    public final static String SEARCH_RESULTS_TOTAL = "totalResults";
+
+    public final static String COLLECTION_BOOKMARK_CHANGED_IS_BOOKMARKED = "isBookmarked";
+
+    public final static String COLLECTION_VISIBILITY_CHANGED_IS_DISCOVERABLE = "isDiscoverable";
+
+    public final static String COLLECTION_SUBJECTS_CHANGED_SUBJECTS = "subjects";
+
+    public final static String COLLECTION_AGE_RANGE_CHANGED_RANGE_MIN = "rangeMin";
+    public final static String COLLECTION_AGE_RANGE_CHANGED_RANGE_MAX = "rangeMax";
 }
--- event-service/src/main/kotlin/com/boclips/event/service/domain/EventSerializer.kt
+++ event-service/src/main/kotlin/com/boclips/event/service/domain/EventSerializer.kt
@@ -1,8 +1,37 @@
 package com.boclips.event.service.domain
 
-import com.boclips.event.infrastructure.EventFields
+import com.boclips.event.infrastructure.EventFields.COLLECTION_AGE_RANGE_CHANGED_RANGE_MAX
+import com.boclips.event.infrastructure.EventFields.COLLECTION_AGE_RANGE_CHANGED_RANGE_MIN
+import com.boclips.event.infrastructure.EventFields.COLLECTION_BOOKMARK_CHANGED_IS_BOOKMARKED
+import com.boclips.event.infrastructure.EventFields.COLLECTION_ID
+import com.boclips.event.infrastructure.EventFields.COLLECTION_SUBJECTS_CHANGED_SUBJECTS
+import com.boclips.event.infrastructure.EventFields.COLLECTION_VISIBILITY_CHANGED_IS_DISCOVERABLE
+import com.boclips.event.infrastructure.EventFields.DEVICE_ID
+import com.boclips.event.infrastructure.EventFields.EXTERNAL_USER_ID
+import com.boclips.event.infrastructure.EventFields.ORGANISATION_ID
+import com.boclips.event.infrastructure.EventFields.ORGANISATION_PARENT_ID
+import com.boclips.event.infrastructure.EventFields.ORGANISATION_PARENT_TYPE
+import com.boclips.event.infrastructure.EventFields.ORGANISATION_TYPE
+import com.boclips.event.infrastructure.EventFields.PAYLOAD
+import com.boclips.event.infrastructure.EventFields.PLAYBACK_SEGMENT_END_SECONDS
+import com.boclips.event.infrastructure.EventFields.PLAYBACK_SEGMENT_START_SECONDS
+import com.boclips.event.infrastructure.EventFields.PLAYBACK_VIDEO_INDEX
+import com.boclips.event.infrastructure.EventFields.PLAYER_INTERACTED_WITH_CURRENT_TIME
+import com.boclips.event.infrastructure.EventFields.SEARCH_QUERY
+import com.boclips.event.infrastructure.EventFields.SEARCH_RESOURCE_TYPE
+import com.boclips.event.infrastructure.EventFields.SEARCH_RESULTS_PAGE_INDEX
+import com.boclips.event.infrastructure.EventFields.SEARCH_RESULTS_PAGE_RESOURCE_IDS
+import com.boclips.event.infrastructure.EventFields.SEARCH_RESULTS_PAGE_SIZE
+import com.boclips.event.infrastructure.EventFields.SEARCH_RESULTS_PAGE_VIDEO_IDS
+import com.boclips.event.infrastructure.EventFields.SEARCH_RESULTS_TOTAL
+import com.boclips.event.infrastructure.EventFields.SUBTYPE
+import com.boclips.event.infrastructure.EventFields.TIMESTAMP
+import com.boclips.event.infrastructure.EventFields.TYPE
+import com.boclips.event.infrastructure.EventFields.Type
+import com.boclips.event.infrastructure.EventFields.URL
+import com.boclips.event.infrastructure.EventFields.USER_ID
+import com.boclips.event.infrastructure.EventFields.VIDEO_ID
 import com.boclips.eventbus.events.base.AbstractCollectionEvent
-import com.boclips.eventbus.events.base.AbstractEventWithUser
 import com.boclips.eventbus.events.base.AbstractEventWithUserId
 import com.boclips.eventbus.events.collection.CollectionAgeRangeChanged
 import com.boclips.eventbus.events.collection.CollectionBookmarkChanged
@@ -24,149 +53,147 @@ import java.util.Date
 object EventSerializer {
 
     fun convertVideosSearched(videosSearched: VideosSearched): Map<String, Any> {
-        return convertUserEvent(videosSearched, type = "VIDEOS_SEARCHED") +
-            ("query" to videosSearched.query) +
-            ("pageIndex" to videosSearched.pageIndex) +
-            ("pageSize" to videosSearched.pageSize) +
-            ("pageVideoIds" to videosSearched.pageVideoIds) +
-            ("totalResults" to videosSearched.totalResults)
+        return convertUserEvent(videosSearched, type = Type.VIDEOS_SEARCHED) +
+            (SEARCH_QUERY to videosSearched.query) +
+            (SEARCH_RESULTS_PAGE_INDEX to videosSearched.pageIndex) +
+            (SEARCH_RESULTS_PAGE_SIZE to videosSearched.pageSize) +
+            (SEARCH_RESULTS_PAGE_VIDEO_IDS to videosSearched.pageVideoIds) +
+            (SEARCH_RESULTS_TOTAL to videosSearched.totalResults)
     }
 
     fun convertVideoSegmentPlayed(videoSegmentPlayed: VideoSegmentPlayed): Map<String, Any> {
-        return convertUserEvent(videoSegmentPlayed, "VIDEO_SEGMENT_PLAYED") +
-            ("segmentStartSeconds" to videoSegmentPlayed.segmentStartSeconds) +
-            ("segmentEndSeconds" to videoSegmentPlayed.segmentEndSeconds) +
-            ("videoIndex" to videoSegmentPlayed.videoIndex) +
-            ("videoId" to videoSegmentPlayed.videoId) +
-            (EventFields.DEVICE_ID to (videoSegmentPlayed.deviceId ?: videoSegmentPlayed.playbackDevice))
+        return convertUserEvent(videoSegmentPlayed, Type.VIDEO_SEGMENT_PLAYED) +
+            (PLAYBACK_SEGMENT_START_SECONDS to videoSegmentPlayed.segmentStartSeconds) +
+            (PLAYBACK_SEGMENT_END_SECONDS to videoSegmentPlayed.segmentEndSeconds) +
+            (PLAYBACK_VIDEO_INDEX to videoSegmentPlayed.videoIndex) +
+            (VIDEO_ID to videoSegmentPlayed.videoId) +
+            (DEVICE_ID to (videoSegmentPlayed.deviceId ?: videoSegmentPlayed.playbackDevice))
     }
 
     fun convertVideoPlayerInteractedWith(videoPlayerInteractedWith: VideoPlayerInteractedWith): Map<String, Any> {
-        return convertUserEvent(videoPlayerInteractedWith, "VIDEO_PLAYER_INTERACTED_WITH") +
-            ("videoId" to videoPlayerInteractedWith.videoId) +
-            ("currentTime" to videoPlayerInteractedWith.currentTime) +
-            ("subtype" to videoPlayerInteractedWith.subtype) +
-            ("payload" to videoPlayerInteractedWith.payload)
+        return convertUserEvent(videoPlayerInteractedWith, Type.VIDEO_PLAYER_INTERACTED_WITH) +
+            (VIDEO_ID to videoPlayerInteractedWith.videoId) +
+            (PLAYER_INTERACTED_WITH_CURRENT_TIME to videoPlayerInteractedWith.currentTime) +
+            (SUBTYPE to videoPlayerInteractedWith.subtype) +
+            (PAYLOAD to videoPlayerInteractedWith.payload)
     }
 
     fun convertVideoAddedToCollection(videoAddedToCollection: VideoAddedToCollection): Map<String, Any> {
-        return convertUserEvent(videoAddedToCollection, "VIDEO_ADDED_TO_COLLECTION") +
-            ("videoId" to videoAddedToCollection.videoId) +
-            ("collectionId" to videoAddedToCollection.collectionId)
+        return convertUserEvent(videoAddedToCollection, Type.VIDEO_ADDED_TO_COLLECTION) +
+            (VIDEO_ID to videoAddedToCollection.videoId) +
+            (COLLECTION_ID to videoAddedToCollection.collectionId)
     }
 
     fun convertVideoRemovedFromCollection(videoRemovedFromCollection: VideoRemovedFromCollection): Map<String, Any> {
-        return convertUserEvent(videoRemovedFromCollection, "VIDEO_REMOVED_FROM_COLLECTION") +
-            ("videoId" to videoRemovedFromCollection.videoId) +
-            ("collectionId" to videoRemovedFromCollection.collectionId)
+        return convertUserEvent(videoRemovedFromCollection, Type.VIDEO_REMOVED_FROM_COLLECTION) +
+            (VIDEO_ID to videoRemovedFromCollection.videoId) +
+            (COLLECTION_ID to videoRemovedFromCollection.collectionId)
     }
 
     fun convertVideoInteractedWith(event: VideoInteractedWith): Map<String, Any> {
-        return convertUserEvent(event, "VIDEO_INTERACTED_WITH") +
-            ("videoId" to event.videoId) +
-            ("subtype" to event.subtype) +
-            ("payload" to event.payload)
+        return convertUserEvent(event, Type.VIDEO_INTERACTED_WITH) +
+            (VIDEO_ID to event.videoId) +
+            (SUBTYPE to event.subtype) +
+            (PAYLOAD to event.payload)
     }
 
     fun convertCollectionBookmarkChanged(event: CollectionBookmarkChanged): Map<String, Any> {
-        return convertCollectionEvent(event, "COLLECTION_BOOKMARK_CHANGED") +
-            ("isBookmarked" to event.isBookmarked)
+        return convertCollectionEvent(event, Type.COLLECTION_BOOKMARK_CHANGED) +
+            (COLLECTION_BOOKMARK_CHANGED_IS_BOOKMARKED to event.isBookmarked)
     }
 
     fun convertCollectionVisibilityChanged(event: CollectionVisibilityChanged): Map<String, Any> {
-        return convertCollectionEvent(event, "COLLECTION_VISIBILITY_CHANGED") +
-            ("isDiscoverable" to event.isDiscoverable)
+        return convertCollectionEvent(event, Type.COLLECTION_VISIBILITY_CHANGED) +
+            (COLLECTION_VISIBILITY_CHANGED_IS_DISCOVERABLE to event.isDiscoverable)
     }
 
     fun convertCollectionSubjectsChanged(event: CollectionSubjectsChanged): Map<String, Any> {
-        return convertCollectionEvent(event, "COLLECTION_SUBJECTS_CHANGED") +
-            ("subjects" to event.subjects.toList())
+        return convertCollectionEvent(event, Type.COLLECTION_SUBJECTS_CHANGED) +
+            (COLLECTION_SUBJECTS_CHANGED_SUBJECTS to event.subjects.toList())
     }
 
     fun convertCollectionAgeRangeChanged(event: CollectionAgeRangeChanged): Map<String, Any> {
-        return convertCollectionEvent(event, "COLLECTION_AGE_RANGE_CHANGED") +
-            ("rangeMin" to event.rangeMin) +
-            ("rangeMax" to event.rangeMax)
+        return convertCollectionEvent(event, Type.COLLECTION_AGE_RANGE_CHANGED) +
+            (COLLECTION_AGE_RANGE_CHANGED_RANGE_MIN to event.rangeMin) +
+            (COLLECTION_AGE_RANGE_CHANGED_RANGE_MAX to event.rangeMax)
     }
 
     private fun convertCollectionEvent(event: AbstractCollectionEvent, type: String): Map<String, Any> {
-        return convertUserEvent(event, type) + ("collectionId" to event.collectionId)
+        return convertUserEvent(event, type) + (COLLECTION_ID to event.collectionId)
     }
 
     fun convertUserEvent(event: AbstractEventWithUserId, type: String): Map<String, Any> {
         return mapOf<String, Any>(
-            "type" to type,
-            "userId" to event.userId,
-            EventFields.EXTERNAL_USER_ID to (event.externalUserId ?: event.overrideUserId),
-            EventFields.DEVICE_ID to event.deviceId,
-            "timestamp" to Date.from(event.timestamp.toInstant()),
-            "url" to event.url
-        )
-    }
-
-    private fun convertUserEvent(event: AbstractEventWithUser, type: String): Map<String, Any> {
-        return mapOf<String, Any>(
-            "type" to type,
-            "userId" to event.user.id,
-            "timestamp" to Date.from(event.timestamp.toInstant()),
-            "url" to event.url
+            TYPE to type,
+            USER_ID to event.userId,
+            EXTERNAL_USER_ID to (event.externalUserId ?: event.overrideUserId),
+            DEVICE_ID to event.deviceId,
+            TIMESTAMP to Date.from(event.timestamp.toInstant()),
+            URL to event.url
         )
     }
 
     fun convertPageRendered(event: PageRendered): Map<String, Any> {
         return mapOf<String, Any>(
-            "userId" to event.userId,
-            "timestamp" to Date.from(event.timestamp.toInstant()),
-            "url" to event.url,
-            "type" to "PAGE_RENDERED"
+            USER_ID to event.userId,
+            TIMESTAMP to Date.from(event.timestamp.toInstant()),
+            URL to event.url,
+            TYPE to Type.PAGE_RENDERED
         )
     }
 
     fun convertCollectionInteractedWith(event: CollectionInteractedWith): Map<String, Any> {
-        return convertUserEvent(event, "COLLECTION_INTERACTED_WITH") +
-            ("collectionId" to event.collectionId) +
-            ("subtype" to event.subtype.toString())
+        return convertUserEvent(event, Type.COLLECTION_INTERACTED_WITH) +
+            (COLLECTION_ID to event.collectionId) +
+            (SUBTYPE to event.subtype.toString())
     }
 
     fun convertUserExpired(event: UserExpired): Map<String, Any> {
-        return convertUserEvent(event, "USER_EXPIRED").let {
-            if (event.user.organisation == null) {
-                return@let it
-            }
 
-            var extra = mapOf(
-                ("organisationId" to event.user.organisation.id),
-                ("organisationType" to event.user.organisation.type)
+        val coreFields = mapOf<String, Any>(
+            TYPE to Type.USER_EXPIRED,
+            USER_ID to event.user.id,
+            TIMESTAMP to Date.from(event.timestamp.toInstant()),
+            URL to event.url
+        )
+
+        val organisationFields = if (event.user.organisation != null) {
+            mapOf(
+                ORGANISATION_ID to event.user.organisation.id,
+                ORGANISATION_TYPE to event.user.organisation.type
             )
+        } else emptyMap()
 
-            if (event.user.organisation.parent != null) {
-                extra = extra +
-                    ("organisationParentId" to event.user.organisation.parent.id) +
-                    ("organisationParentType" to event.user.organisation.parent.type)
-            }
-            return@let it + extra
-        }
+        val parentOrganisationFields = if (event.user.organisation?.parent != null) {
+            mapOf(
+                ORGANISATION_PARENT_ID to event.user.organisation.parent.id,
+                ORGANISATION_PARENT_TYPE to event.user.organisation.parent.type
+            )
+        } else emptyMap()
+
+        return coreFields + organisationFields + parentOrganisationFields
     }
 
     fun convertResourcesSearched(event: ResourcesSearched): Map<String, Any> {
-        return convertUserEvent(event, type = "RESOURCES_SEARCHED") +
-            ("query" to event.query) +
-            ("pageIndex" to event.pageIndex) +
-            ("pageSize" to event.pageSize) +
-            ("pageResourceIds" to event.pageResourceIds) +
-            ("totalResults" to event.totalResults) +
-            ("resourceType" to event.resourceType)
+        return convertUserEvent(event, type = Type.RESOURCES_SEARCHED) +
+            (SEARCH_QUERY to event.query) +
+            (SEARCH_RESULTS_PAGE_INDEX to event.pageIndex) +
+            (SEARCH_RESULTS_PAGE_SIZE to event.pageSize) +
+            (SEARCH_RESULTS_PAGE_RESOURCE_IDS to event.pageResourceIds) +
+            (SEARCH_RESULTS_TOTAL to event.totalResults) +
+            (SEARCH_RESOURCE_TYPE to event.resourceType)
     }
 
     fun convertPlatformInteractedWith(event: PlatformInteractedWith): Map<String, Any> {
         val convertedEvent = mapOf<String, Any>(
-            "type" to "PLATFORM_INTERACTED_WITH",
-            "timestamp" to Date.from(event.timestamp.toInstant()),
-            "url" to event.url,
-            "subtype" to event.subtype)
+            TYPE to Type.PLATFORM_INTERACTED_WITH,
+            TIMESTAMP to Date.from(event.timestamp.toInstant()),
+            URL to event.url,
+            SUBTYPE to event.subtype
+        )
 
         if (event.userId != null) {
-            return convertedEvent + ("userId" to event.userId)
+            return convertedEvent + (USER_ID to event.userId)
         }
 
         return convertedEvent
