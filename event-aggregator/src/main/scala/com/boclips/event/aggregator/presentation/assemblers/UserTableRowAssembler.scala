38407721bdcd060e03b39c0c8f093fcc535d2c01
Sebastian Kazenbroot-Guppy <sebastian@boclips.com>
2020-10-26 11:45:41 +0000

    Trim historical user data.
    - testing out if this will unblock bigquery loading of user data
    
    Signed-off-by: Raul Vences Paradela <raul@boclips.com>

--- event-aggregator/src/main/scala/com/boclips/event/aggregator/presentation/assemblers/UserTableRowAssembler.scala
+++ event-aggregator/src/main/scala/com/boclips/event/aggregator/presentation/assemblers/UserTableRowAssembler.scala
@@ -1,6 +1,6 @@
 package com.boclips.event.aggregator.presentation.assemblers
 
-import java.time.{LocalDate, YearMonth}
+import java.time._
 
 import com.boclips.event.aggregator._
 import com.boclips.event.aggregator.domain.model.playbacks.Playback
@@ -11,29 +11,40 @@ import com.boclips.event.aggregator.presentation.model.UserTableRow
 import org.apache.spark.rdd.RDD
 
 object UserTableRowAssembler {
+  def userDataCutoffTime: ZonedDateTime =
+    ZonedDateTime.ofLocal(
+      LocalDateTime.of(2020, 9, 1, 0, 0),
+      ZoneId.of("UTC"),
+      ZoneOffset.UTC
+    )
 
   def apply(users: RDD[User], playbacks: RDD[Playback], searches: RDD[Search], sessions: RDD[Session]): RDD[UserTableRow] = {
 
     val playbacksByUser: RDD[(UserIdentity, Iterable[Playback])] = playbacks
+      .filter(_.timestamp.isAfter(userDataCutoffTime))
       .keyBy(_.user)
       .groupByKey()
 
     val referredPlaybacksByUser: RDD[(UserIdentity, Iterable[Playback])] = playbacks
+      .filter(_.timestamp.isAfter(userDataCutoffTime))
       .filter(p => p.refererId.isDefined && p.isShare)
       .keyBy[UserIdentity](p => BoclipsUserIdentity(p.refererId.get))
       .groupByKey()
 
     val searchesByUser: RDD[(UserIdentity, Iterable[Search])] = searches
+      .filter(_.request.timestamp.isAfter(userDataCutoffTime))
       .keyBy[UserIdentity](it => it.request.userIdentity)
       .groupByKey()
 
     val activeMonthsByUser: RDD[(UserIdentity, Iterable[YearMonth])] = playbacks
+      .filter(_.timestamp.isAfter(userDataCutoffTime))
       .filter(p => p.user.id.isDefined)
       .map(playback => (playback.user, YearMonth.from(playback.timestamp)))
       .distinct()
       .groupByKey()
 
     val sessionsByUser: RDD[(UserIdentity, Iterable[Session])] = sessions
+      .filter(_.start.isAfter(userDataCutoffTime))
       .filter(p => p.user.id.isDefined)
       .keyBy(_.user)
       .groupByKey()
