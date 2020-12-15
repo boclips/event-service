3fc410a3f1a7ce23c072d130299ba306653eb9b4
Matt Jones <matt@boclips.com>
2020-12-15 12:34:17 +0000

    Make event service to deal with delivered orders
    [#175055626]
    
    Signed-off-by: Raul Vences Paradela <raul@boclips.com>

--- event-aggregator/src/test/scala/com/boclips/event/aggregator/infrastructure/mongo/MongoOrderLoaderTest.scala
+++ event-aggregator/src/test/scala/com/boclips/event/aggregator/infrastructure/mongo/MongoOrderLoaderTest.scala
@@ -6,15 +6,18 @@ import org.apache.spark.sql.SparkSession
 
 class MongoOrderLoaderTest extends IntegrationTest {
 
-  it should "load orders with status COMPLETED" in mongoSparkTest { (spark: SparkSession, mongo) =>
+  it should "load orders with status COMPLETED and DELIVERED" in mongoSparkTest { (spark: SparkSession, mongo) =>
     val collection = mongo.collection[OrderDocument]("orders")
     collection insertOne OrderDocument.sample.id("order-1").status("COMPLETED").build()
     collection insertOne OrderDocument.sample.id("order-2").status("CANCELLED").build()
+    collection insertOne OrderDocument.sample.id("order-3").status("DELIVERED").build()
 
     val orders = new MongoOrderLoader(mongo).load()(spark).toLocalIterator.toList
+    val orderIds = orders.map(_.id.value)
 
-    orders should have size 1
-    orders.head.id.value shouldBe ("order-1")
+    orders should have size 2
+    orderIds should contain ("order-1")
+    orderIds should contain ("order-3")
   }
 
 }
