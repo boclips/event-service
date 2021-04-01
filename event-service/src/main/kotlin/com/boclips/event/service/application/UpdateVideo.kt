f480e1c0333d5463e964544e02f1bb9db70185fc
Michal Wit <michal@boclips.com>
2021-04-01 16:41:59 +0200

    Log info about processing VideoBroadcastRequested event
    [#176803131]

--- event-service/src/main/kotlin/com/boclips/event/service/application/UpdateVideo.kt
+++ event-service/src/main/kotlin/com/boclips/event/service/application/UpdateVideo.kt
@@ -6,6 +6,7 @@ import com.boclips.eventbus.domain.video.Video
 import com.boclips.eventbus.events.video.VideoBroadcastRequested
 import com.boclips.eventbus.events.video.VideoCreated
 import com.boclips.eventbus.events.video.VideoUpdated
+import mu.KLogging
 
 class UpdateVideo(private val videoRepository: VideoRepository) {
 
@@ -21,10 +22,19 @@ class UpdateVideo(private val videoRepository: VideoRepository) {
 
     @BoclipsEventListener
     fun videoBroadcastRequested(event: VideoBroadcastRequested) {
+        logger.info {
+            "Processing VideoBroadcastRequested event for video with ID ${event.video.id}." +
+                " Source video reference: ${event.video.sourceVideoReference}"
+        }
         saveVideo(event.video)
+        logger.info {
+            "Processing VideoBroadcastRequested event for video with ID (${event.video.id}) finished."
+        }
     }
 
     private fun saveVideo(video: Video) {
         videoRepository.saveVideo(video)
     }
+
+    companion object : KLogging()
 }
